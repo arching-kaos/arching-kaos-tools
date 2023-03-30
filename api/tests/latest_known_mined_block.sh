4f1df24786d4be95e0029e6c142ce376f98377dc
kaotisk <kaotisk@arching-kaos.org>
2023-03-30 03:18:43 +0300

    Fixed test command

--- api/tests/latest_known_mined_block.sh
+++ api/tests/latest_known_mined_block.sh
@@ -3,7 +3,7 @@
 printf "TEST\t/slatest\n"
 printf "\t01:\tendpoint\n"
 A="$(curl http://127.0.0.1:8610/v0/slatest 2>/dev/null)"
-B="$(bash /home/$USER/projects/arching-kaos-tools/find-latest-mined-block.sh)"
+B="$(ak-find-latest-mined-block)"
 if [ "$A" == "$B" ]
 then
     printf '\t\t\033[0;32mPASSED\033[0;0m'
