21f76a6dbf6fab5311ab7f4b7b2fb5d0991ea3b2
kaotisk <kaotisk@arching-kaos.org>
2023-04-03 05:18:04 +0300

    Attempt to create cache places for the API to hit instead of running things

---
+++ api/make_cache.sh
@@ -0,0 +1,6 @@
+AK_CACHE="$AK_WORKDIR/cache"
+
+ak-find-latest-mined-sblock > $AK_CACHE/ak-find-latest-mined-sblock.json
+ak-get-chain-minified > $AK_CACHE/ak-get-chain-minified.json
+ak-get-latest > $AK_CACHE/ak-get-latest.json
+
