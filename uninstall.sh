2e5a5246016724aca4f3bbc2d7418542317a5147
kaotisk <kaotisk@arching-kaos.org>
2023-08-20 13:01:40 +0300

    (new) uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+printf "Arching Kaos Tools - Uninstaller\n"
+printf "--------------------------------\n"
+printf "Backing up your GPG keyring..."
+tar cvfz $AK_ROOT/aknet-gpg-keyring-backup-`date -u +%Y%m%d`.tar.gz $AK_GPGHOME
+if [ $? -ne 0 ]
+then
+    printf "\tFailed! Aborting...\n"
+    exit 1
+fi
+printf "\tOK!\n"
+
+printf "Removing %s..." "$AK_WORKDIR"
+rm -rf $AK_WORKDIR
+if [ $? -ne 0 ]
+then
+    printf "\tFailed! Aborting...\n"
+    exit 1
+fi
+printf "\tOK!\n"
+
+printf "Unsetting sourcing from .bashrc ..."
+sed -i.bak -e 's/source.*arching-kaos\/rc//g' $HOME/.bashrc
+if [ $? -ne 0 ]
+then
+    printf "\tFailed! Aborting...\n"
+    exit 1
+fi
+printf "\tOK!\n"
+
+printf "Resourcing .bashrc..."
+source $HOME/.bashrc
+if [ $? -ne 0 ]
+then
+    printf "\tFailed! Aborting...\n"
+    exit 1
+fi
+printf "\tOK!\n"
