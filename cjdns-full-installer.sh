d744e9daee83ce703405a4ba4d0303daed0d1527
kaotisk <kaotisk@arching-kaos.org>
2024-02-13 02:26:07 +0200

    Clone and install cjdns script

---
+++ cjdns-full-installer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+git clone https://github.com/cjdelisle/cjdns
+cd cjdns
+./do
+if [ $? -ne 0 ]
+then
+    echo "Failed to compile cjdns"
+    exit 1
+fi
+cd ..
+sudo cp cjdns/cjdroute /usr/bin/
+sudo cp cjdns/target/release/makekeys /usr/bin/
+sudo cp cjdns/target/release/mkpasswd /usr/bin/
+sudo cp cjdns/target/release/privatetopublic /usr/bin/
+sudo cp cjdns/target/release/publictoip6 /usr/bin/
+sudo cp cjdns/target/release/randombytes /usr/bin/
+sudo cp cjdns/target/release/sybilsim /usr/bin/
+sudo cp cjdns/contrib/systemd/cjdns.service /etc/systemd/system/
+sudo cp cjdns/contrib/systemd/cjdns-resume.service /etc/systemd/system
+sudo systemctl enable --now cjdns.service
