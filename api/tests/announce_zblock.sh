c0410df04f57c166c79b8bbe584a97abadaed36e
kaotisk <kaotisk@arching-kaos.org>
2024-03-01 02:57:58 +0200

    Fixes in output format

--- api/tests/announce_zblock.sh
+++ api/tests/announce_zblock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PROGRAM="$(basename $0)"
 printf '[%s]\n' "$PROGRAM"
+printf "TEST\t/v0/announce/zblock\n"
 printf "\t01:\tendpoint with valid data"
 curl http://127.0.0.1:8610/v0/announce/zblock --header 'Content-Type: application/json' --data-raw '{"zblock":"'$(ak-get-zlatest)'"}' 2>/dev/null | jq -M -c > /dev/null
 if [ "$?" == "0" ]
@@ -25,8 +26,8 @@ printf "\t03:\tendpoint no data"
 curl http://127.0.0.1:8610/v0/announce/zblock --header 'Content-Type: application/json' --data-raw '{"zblock":""}' 2>/dev/null | jq -M -c > /dev/null
 if [ "$?" == "0" ]
 then
-    printf "\t\t\033[0;32mPASSED\033[0;0m"
+    printf "\t\t\t\033[0;32mPASSED\033[0;0m"
 else
-    printf "\t\033[0;31mFAILED\033[0;0m"
+    printf "\t\t\033[0;31mFAILED\033[0;0m"
 fi
 printf "\n"
--- api/tests/announce_zchain_link.sh
+++ api/tests/announce_zchain_link.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROGRAM="$(basename $0)"
 printf '[%s]\n' "$PROGRAM"
-printf "TEST\t/announce/zchain\n"
+printf "TEST\t/v0/announce/zchain\n"
 # curl http://127.0.0.1:8610/v0/announce/zchain
 printf "\t01:\tendpoint with valid data"
 curl -POST http://localhost:8610/v0/announce/zchain --header 'Content-Type: application/json' --data-raw '{"zchain":"k51qzi5uqu5dgapvk7bhxmchuqya9immqdpbz0f1r91ckpdqzub63afn3d5apr"}' 2>/dev/null | jq -M -c > /dev/null
@@ -27,8 +27,8 @@ printf "\t03:\tendpoint no data"
 curl -POST http://localhost:8610/v0/announce/zchain --header 'Content-Type: application/json' --data-raw '{"zchain":""}' 2>/dev/null | jq -M -c > /dev/null
 if [ "$?" == "0" ]
 then
-    printf "\t\t\033[0;32mPASSED\033[0;0m"
+    printf "\t\t\t\033[0;32mPASSED\033[0;0m"
 else
-    printf "\t\033[0;31mFAILED\033[0;0m"
+    printf "\t\t\t\033[0;31mFAILED\033[0;0m"
 fi
 printf "\n"
