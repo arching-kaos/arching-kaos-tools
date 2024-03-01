1b883da0cafceeb82883985486752d98299ece0e
kaotisk <kaotisk@arching-kaos.org>
2024-03-01 05:44:05 +0200

    Firstly clean up AK_BINDIR, then make links

--- update.sh
+++ update.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Find scripts and create symlinks
-
-binfiles=$(ls -1 $(pwd)/bin)
-for b in $binfiles
-do
-    if [ ! -L $AK_BINDIR/$b ] ; then ln -s $(pwd)/bin/$b $AK_BINDIR/$b ;fi
-done
 find $AK_BINDIR -type l | while read link
 do
     if [ ! -f $link ]
@@ -14,4 +7,11 @@ do
         echo "Non working link: $(basename $link) removing..."
         rm $link
     fi
+done
+
+# Find scripts and create symlinks
+binfiles=$(ls -1 $(pwd)/bin)
+for b in $binfiles
+do
+    if [ ! -L $AK_BINDIR/$b ] ; then ln -s $(pwd)/bin/$b $AK_BINDIR/$b ;fi
 done
