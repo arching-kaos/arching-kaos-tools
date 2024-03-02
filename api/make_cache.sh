2e2e16eda2c02bf2b783ca5d4c649e17b287e16b
kaotisk <kaotisk@arching-kaos.org>
2024-03-02 05:42:27 +0200

    Removed ak-get-chain-minified in favor of ak-enter

--- api/make_cache.sh
+++ api/make_cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ak-find-latest-mined-sblock > $AK_CACHEDIR/ak-find-latest-mined-sblock.json
-ak-get-chain-minified > $AK_CACHEDIR/ak-get-chain-minified.json
+ak-enter > $AK_CACHEDIR/ak-get-chain-minified.json
 ak-get-zlatest > $AK_CACHEDIR/ak-get-zlatest.json
 
