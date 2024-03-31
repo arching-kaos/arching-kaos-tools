25cabe789a20e39e666dc56348cb32d0d1714dbd
kaotisk <kaotisk@arching-kaos.org>
2024-03-31 13:54:51 +0300

    yeap, broke api for starters

--- api/routes/getNodeInfo/index.js
+++ api/routes/getNodeInfo/index.js
@@ -1,5 +1,5 @@
 const { spawn } = require('child_process');
-const akLogThis = require('../../lib/akLogThis');
+const akLogMessage = require('../../lib/akLogMessage');
 /*
  * Gets the local latest zblock
  *
@@ -9,7 +9,7 @@ const akLogThis = require('../../lib/akLogThis');
  *
  */
 module.exports = (req, res) => {
-    akLogThis('INFO', `Incoming from [${req.socket._peername.address}]:${req.socket._peername.port} @ ${req.get('host')}${req._parsedOriginalUrl.pathname}`);
+    akLogMessage('INFO', `Incoming from [${req.socket._peername.address}]:${req.socket._peername.port} @ ${req.get('host')}${req._parsedOriginalUrl.pathname}`);
     const command = spawn("ak-config", ["get-published"]);
     var buffer = "";
     command.stdout.on("data", data => {
