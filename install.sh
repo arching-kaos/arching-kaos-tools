946d002543083d9561bb1e7f851b8179099ad077
kaotisk <kaotisk@arching-kaos.org>
2024-04-04 09:39:47 +0300

    Deployed space lazers to eliminate tabbed spaces
    - Also: _ak_not_implemented introduced

--- install.sh
+++ install.sh
@@ -161,40 +161,40 @@ if [ ! -d $AK_GPGHOME ]; then mkdir $AK_GPGHOME && chmod 700 -R $AK_GPGHOME; fi
 
 touch $AK_WORKDIR/logs
 logthis(){
-	echo "Install script: $1" >> $AK_WORKDIR/logs
+    echo "Install script: $1" >> $AK_WORKDIR/logs
 }
 
 HAK=".arching-kaos"
 logthis "Searching for $HAK folder and files"
 if [ ! -d $HOME/$HAK ]; then
-	mkdir $HOME/$HAK
-	logthis "$HAK created in $HOME";
+    mkdir $HOME/$HAK
+    logthis "$HAK created in $HOME";
 fi
 logthis "Searching for rc"
 if [ ! -f $HOME/$HAK/rc ]; then
-	echo export PATH=$PATH:$HOME/$HAK/bin > $HOME/$HAK/rc
-	cat config.sh >> $HOME/$HAK/rc
-	logthis "New rc export to file";
+    echo export PATH=$PATH:$HOME/$HAK/bin > $HOME/$HAK/rc
+    cat config.sh >> $HOME/$HAK/rc
+    logthis "New rc export to file";
 fi
 logthis "Searching for shell"
 if [ $SHELL == "/usr/bin/zsh" ]; then
-	SHELLRC=".zshrc"
-	logthis "ZSH found";
+    SHELLRC=".zshrc"
+    logthis "ZSH found";
 elif [ $SHELL == "/usr/bin/bash" ]; then
-	SHELLRC='.bashrc'
-	logthis "BASH found";
+    SHELLRC='.bashrc'
+    logthis "BASH found";
 else
-	logthis "Unknown shell... defaulting to bash"
-	SHELLRC='.bashrc'
+    logthis "Unknown shell... defaulting to bash"
+    SHELLRC='.bashrc'
 fi
 logthis "Searching if rc is already there"
 grep "source $HOME/$HAK/rc" $HOME/$SHELLRC > /dev/null 2>&1
 if [ $? == 0 ]; then
-	logthis "Already installed";
+    logthis "Already installed";
 else
-	echo "source $HOME/$HAK/rc" >> $HOME/$SHELLRC
-	logthis "$HAK installed at $HOME and sourced it in $SHELLRC"
-	source $HOME/$HAK/rc;
+    echo "source $HOME/$HAK/rc" >> $HOME/$SHELLRC
+    logthis "$HAK installed at $HOME and sourced it in $SHELLRC"
+    source $HOME/$HAK/rc;
 fi
 sh ipfs-check-install-setup-init-update
 source ./config.sh
