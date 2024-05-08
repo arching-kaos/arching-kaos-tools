41ff2364108136690cd6ece965e4df70dbe27557
kaotisk <kaotisk@arching-kaos.org>
2024-05-08 23:32:36 +0300

    Output log file per distribution build

--- test_with_podman.sh
+++ test_with_podman.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 podman_dir="podman"
 now="$(date -u +%s)"
-build_log="$now-build_log"
 for file in $podman_dir/*
 do
     distro="$(printf "%s" "$file"|cut -d '.' -f 2)"
+    build_log="$now-podman-$distro-build_log"
     printf "Building %s container..." "$distro"
     podman build -f $file -t arching-kaos-tools-$distro . >> $build_log 2>&1
     if [ $? -ne 0 ]
