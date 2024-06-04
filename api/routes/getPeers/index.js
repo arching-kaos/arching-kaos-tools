2c28d8dd96327db1d51e86eb06573f7597e29cfc
kaotisk <kaotisk@arching-kaos.org>
2024-06-04 17:13:07 +0300

    Refactoring

--- api/routes/getPeers/index.js
+++ api/routes/getPeers/index.js
@@ -1,30 +1,11 @@
-const { spawn } = require('child_process');
 const config = require('../../config');
 const fs = require('fs');
 
 module.exports = (req, res) => {
-//    const command = spawn("ak-config", ["get-published"]);
     const path = config.peersFile;
     if(fs.existsSync(path)){
         res.send(JSON.parse(fs.readFileSync(path)));
     } else {
         res.send({"error":"404"})
     }
-//    var buffer = "";
-//    command.stdout.on("data", data => {
-//        buffer = buffer + data;
-//    });
-//
-//    command.stderr.on("data", data => {
-//            console.log(`stderr: ${data}`);
-//    });
-//
-//    command.on('error', (error) => {
-//            console.log(`error: ${error.message}`);
-//    });
-//
-//    command.on("close", code => {
-//            res.send(JSON.parse(fs.Read));
-//            console.log(`child process exited with code ${code}`);
-//    });
 };
