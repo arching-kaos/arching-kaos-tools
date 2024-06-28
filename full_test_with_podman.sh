f9c0cbe0340e22f9bb11e77b6c0f302364c391db
kaotisk <kaotisk@arching-kaos.org>
2024-06-28 13:35:52 +0300

    Expose port to host so we can test the API (won't work cause of disallowed network, tho :) )

--- full_test_with_podman.sh
+++ full_test_with_podman.sh
@@ -5,7 +5,7 @@ then
     tag="$(bash ./test_with_podman.sh "$1" | tail -n 2 | head -n 1 | cut -d ' ' -f 3)"
     name="$(echo -n $tag | cut -d ':' -f 1 | cut -d '/' -f 2)"
     container="akt-test-$name"
-    podman run --name $container -d $tag
+    podman run --name $container -d -p 8620:8610 $tag
     podman exec -it $container bash
     podman container stop $container
     podman container rm $container
