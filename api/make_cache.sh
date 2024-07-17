cdf6fdb9d0d864e38ff31a3b06402e5c7dd1090d
kaotisk <kaotisk@arching-kaos.org>
2024-07-17 09:25:24 +0300

    removing ak-find-latest-mined-sblock

--- api/make_cache.sh
+++ api/make_cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ak-find-latest-mined-sblock > $AK_CACHEDIR/ak-find-latest-mined-sblock.json
+ak schain --get-latest > $AK_CACHEDIR/ak-find-latest-mined-sblock.json
 ak zchain --crawl > $AK_CACHEDIR/ak-get-chain-minified.json
 ak zchain --get-latest > $AK_CACHEDIR/ak-get-zlatest.json
 
--- api/tests/latest_known_mined_block.sh
+++ api/tests/latest_known_mined_block.sh
@@ -5,7 +5,7 @@ printf '[%s]\n' "$PROGRAM"
 printf "TEST\t/slatest\n"
 printf "\t01:\tendpoint"
 A="$(curl http://127.0.0.1:8610/v0/slatest 2>/dev/null)"
-B="$(ak-find-latest-mined-sblock)"
+B="$(ak schain --get-latest)"
 if [ "$A" == "$B" ]
 then
     printf '\t\t\033[0;32mPASSED\033[0;0m'
