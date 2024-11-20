59afc2bc737f4c9edf32cc1ee05e81b99f0eeb2f
kaotisk <kaotisk@arching-kaos.org>
2024-11-20 11:48:10 +0200

    Implements the following routes to the API
    - /leaf
    - /chunk
    - /map

---
+++ api/routes/getLeaf/index.js
@@ -0,0 +1,35 @@
+/*
+ * Receives an SHA512SUM as a leaf's hash and if exists it
+ * returns the leaf's content
+ *
+ */
+
+const fs = require('fs');
+const config = require("../../config.js");
+
+module.exports = (req, res) => {
+    var args = req.url.split("/");
+    var hash = args[3];
+    regex= /[a-f0-9]{128}/
+    if (regex.test(hash))
+    {
+        var path = config.leafsDir+"/"+hash;
+        try
+        {
+            if(fs.existsSync(path))
+            {
+                res.send(fs.readFileSync(path));
+            }
+        }
+        catch (error)
+        {
+            res.send({"error":error.message});
+        }
+    }
+    else
+    {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({error:"No hash"}));
+    }
+}
+
