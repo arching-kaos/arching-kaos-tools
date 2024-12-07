237a32846ad4a8f8634a68f0a92c71c49807de26
kaotisk <kaotisk@arching-kaos.org>
2024-12-07 02:38:36 +0200

    Load nvm after installing

--- install.sh
+++ install.sh
@@ -96,6 +96,8 @@ do
                 fi
                 printf "\t nvm installed!"
                 printf "\t Installing latest nodejs..."
+                export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+                [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
                 nvm install $(nvm ls-remote|tail -n 1)
                 if [ $? -ne 0 ]
                 then
