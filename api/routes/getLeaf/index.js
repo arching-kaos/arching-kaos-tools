de38fcf7bfbb5b8ba70e54621cb69f0b3aeba65c
kaotisk <kaotisk@arching-kaos.org>
2024-12-13 15:46:45 +0200

    At least reply something

--- api/routes/getLeaf/index.js
+++ api/routes/getLeaf/index.js
@@ -21,6 +21,11 @@ module.exports = (req, res) => {
                 res.writeHead(200, {'Content-Type': 'application/json'});
                 res.end(fs.readFileSync(path));
             }
+            else
+            {
+                res.writeHead(404, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({"error":"not found"}));
+            }
         }
         catch (error)
         {
