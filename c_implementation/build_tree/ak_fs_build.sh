ea3967ec02d8a1abcf35182387f5d220255c346a
kaotisk <kaotisk@arching-kaos.org>
2025-04-24 01:43:10 +0300

    updating a deprecated script

--- c_implementation/build_tree/ak_fs_build.sh
+++ c_implementation/build_tree/ak_fs_build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Building lib/akfs.so" && \
+gcc -c -shared -Wextra -Wall -Werror -pedantic -ggdb -fPIC -I./include ./src/aklog.c -o lib/libaklog.so && \
 gcc -c -shared -Wextra -Wall -Werror -pedantic -ggdb -fPIC -I./include ./src/akfs.c -o lib/libakfs.so && \
 echo "Building tests/test_akfs" && \
-gcc -Wextra -Wall -Werror -pedantic -ggdb -Wl,-rpath=lib -I./include tests/test_akfs.c lib/libakfs.so -o tests/test_akfs && \
+gcc -Wextra -Wall -Werror -pedantic -ggdb -Wl,-rpath=lib -I./include tests/test_akfs.c lib/libakfs.so lib/libaklog.so -o x_test_akfs && \
 echo "Running test_akfs" && \
-time ./tests/test_akfs && \
-rm ./tests/test_akfs
+time ./x_test_akfs && \
+rm ./x_test_akfs
 
-echo "Building tests/test_akfs_mkdir.c" && \
-gcc -Wextra -Wall -Werror -pedantic -ggdb -Wl,-rpath=lib -I./include tests/test_akfs_mkdir.c lib/libakfs.so -o tests/test_akfs_mkdir && \
-echo "Running test_akfs_mkdir" && \
-time ./tests/test_akfs_mkdir && \
-rm ./tests/test_akfs_mkdir
+# echo "Building tests/test_akfs_mkdir.c" && \
+# gcc -Wextra -Wall -Werror -pedantic -ggdb -Wl,-rpath=lib -I./include tests/test_akfs_mkdir.c lib/libakfs.so -o tests/test_akfs_mkdir && \
+# echo "Running test_akfs_mkdir" && \
+# time ./tests/test_akfs_mkdir && \
+# rm ./tests/test_akfs_mkdir
