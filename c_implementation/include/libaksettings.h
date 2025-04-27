d8808f1b35490e8929cea7b083eb4b2546619b59
kaotisk <kaotisk@arching-kaos.org>
2025-04-27 07:04:55 +0300

    [libaksettings] updates

--- c_implementation/include/libaksettings.h
+++ c_implementation/include/libaksettings.h
@@ -8,18 +8,16 @@ typedef struct {
     char *value;
 } AKSetting;
 
-int ak_settings();
 void ak_settings_print_setting(AKSetting);
 int ak_settings_from_file();
 int ak_setting_to_file(AKSetting);
 
 AKSetting* ak_settings_get_all();
 
-char *ak_settings_get_setting(const char *key);
-bool ak_settings_set_setting(const char *key, const char *value);
+char *ak_settings_get_setting(const char*);
+bool ak_settings_set_setting(const char*, const char*);
 bool ak_settings_save_settings();
 bool ak_settings_load_settings_binary();
-int ak_settings_find_setting(const char *key);
 void ak_settings_free_settings();
 bool ak_settings_save_settings_binary();
 void ak_settings_import_from_environment();
